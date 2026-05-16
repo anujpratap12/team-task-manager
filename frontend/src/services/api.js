36e3a3792f3652f969912070e36a5cd32169384a
Anuj Pratap Singh <apsrajput183@gmail.com>
2026-05-16 11:50:54 +0530

    Connected frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-production-812e.up.railway.app/api",
 })
 
 // Add token automatically
